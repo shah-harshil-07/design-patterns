c409448ff1a43781d001f167835f7989ea462612
Harshil Shah <shahharshil1998@gmail.com>
2025-11-10 18:28:03 +0530

    Added `Composite` and `Facade` design pattern.

---
+++ src/Composite/Main.java
@@ -0,0 +1,94 @@
+import java.util.*;
+
+interface CartItem {
+    double getPrice();
+    void display(String indent);
+}
+
+// Represents a single product
+class Product implements CartItem {
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void display(String indent) {
+        System.out.println(indent + "Product: " + name + " - ₹" + price);
+    }
+}
+
+// Represents a bundle of products
+class ProductBundle implements CartItem {
+    private String bundleName;
+    private List<Product> products = new ArrayList<>();
+
+    public ProductBundle(String bundleName) {
+        this.bundleName = bundleName;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public double getPrice() {
+        double total = 0;
+        for (Product product : products) {
+            total += product.getPrice();
+        }
+
+        return total;
+    }
+
+    public void display(String indent) {
+        System.out.println(indent + "Bundle: " + bundleName);
+        for (Product product : products) {
+            product.display(indent + "  ");
+        }
+    }
+}
+
+// Main logic
+class Main {
+    public static void main(String[] args) {
+        // Individual Items
+        Product book = new Product("Book", 500);
+        Product headphones = new Product("Headphones", 1500);
+        Product charger = new Product("Charger", 800);
+        Product pen = new Product("Pen", 20);
+        Product notebook = new Product("Notebook", 60);
+
+        // Bundle: Iphone Combo
+        ProductBundle iphoneCombo = new ProductBundle("iPhone Combo Pack");
+        iphoneCombo.addProduct(headphones);
+        iphoneCombo.addProduct(charger);
+
+        // Bundle: School Kit
+        ProductBundle schoolKit = new ProductBundle("School Kit");
+        schoolKit.addProduct(pen);
+        schoolKit.addProduct(notebook);
+
+        // Add to cart logic
+        List<CartItem> cart = new ArrayList<>();
+        cart.add(book);
+        cart.add(iphoneCombo);
+        cart.add(schoolKit);
+
+        // Display Cart
+        double total = 0;
+        System.out.println("Cart Details:\n");
+
+        for (CartItem item : cart) {
+            item.display("  ");
+            total += item.getPrice();
+        }
+
+        System.out.println("\nTotal Price: ₹" + total);
+    }
+}
---
+++ src/Facade/Main.java
@@ -0,0 +1,77 @@
+class PaymentService {
+    public void makePayment(String accountId, double amount) {
+        System.out.println("Payment made of $" + amount + " by account: " + accountId);
+    }
+}
+
+class SeatReservationService {
+    public void reserveSeat(String movieId, String seatNo) {
+        System.out.println("Seat no. " + seatNo + " booked for movie: " + movieId);
+    }
+}
+
+class NotificationService {
+    public void notifyUser(String userEmail) {
+        System.out.println("Booking email sent to the user on email: " + userEmail);
+    }
+}
+
+class LoyaltyPointsService {
+    public void generateLoyaltyPoints(String accountId, int points) {
+        System.out.println(points + " points generated for account: " + accountId);
+    }
+}
+
+class TicketBookingService {
+    public void bookTicket(String movieId, String seatNo, String accountId) {
+        System.out.println("Ticket booked on seat: " + seatNo + " booked for movie: " + movieId + " by account: " + accountId);
+    }
+}
+
+class MovieBookingFacade {
+    private PaymentService paymentService;
+    private NotificationService notificationService;
+    private LoyaltyPointsService loyaltyPointsService;
+    private TicketBookingService ticketBookingService;
+    private SeatReservationService seatReservationService;
+
+    public MovieBookingFacade() {
+        this.paymentService = new PaymentService();
+        this.notificationService = new NotificationService();
+        this.loyaltyPointsService = new LoyaltyPointsService();
+        this.ticketBookingService = new TicketBookingService();
+        this.seatReservationService = new SeatReservationService();
+    }
+    
+
+    public void bookMovie(
+        String accountId,
+        String movieId,
+        String seatNo,
+        double amount,
+        String userEmail,
+        int points
+    ) {
+        this.paymentService.makePayment(accountId, amount);
+        this.seatReservationService.reserveSeat(movieId, seatNo);
+        this.ticketBookingService.bookTicket(movieId, seatNo, accountId);
+        this.notificationService.notifyUser(userEmail);
+        this.loyaltyPointsService.generateLoyaltyPoints(accountId, points);
+
+        System.out.println("Movie ticket booking completed successfully!");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        MovieBookingFacade movieBookingFacade = new MovieBookingFacade();
+        movieBookingFacade.bookMovie(
+            "account123",
+            "movie123",
+            "A10",
+            405.81,
+            "shahharshil1998@gmail.com",
+            40
+        );
+    }
+}
