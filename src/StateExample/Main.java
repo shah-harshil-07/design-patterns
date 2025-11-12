5dd497a0f0de14e38f2093c4e56f3e33ab4d892f
Harshil Shah <shahharshil1998@gmail.com>
2025-11-12 18:25:21 +0530

    Added `State` and `Template` design patterns.

---
+++ src/StateExample/Main.java
@@ -0,0 +1,146 @@
+import java.util.*;
+
+class OrderContext {
+    private OrderState currentState;
+
+    public OrderContext() {
+        this.currentState = new OrderPlacedState();
+    }
+
+    public void setCurrentState(OrderState state) {
+        this.currentState = state;
+    }
+
+    public void next() {
+        currentState.next(this);
+    }
+
+    public void cancel() {
+        currentState.cancel(this);
+    }
+
+    public String getCurrentState() {
+        return currentState.getName();
+    }
+}
+
+interface OrderState {
+    public String getName();
+    public void next(OrderContext order);
+    public void cancel(OrderContext order);
+}
+
+class OrderPlacedState implements OrderState {
+    @Override
+    public void next(OrderContext context) {
+        context.setCurrentState(new PreparingFoodState());
+        System.out.println("Order status changed to `Preparing food`");
+    }
+
+    @Override
+    public void cancel(OrderContext context) {
+        context.setCurrentState(new OrderCancelledState());
+        System.out.println("Order cancelled!!");
+    }
+
+    @Override
+    public String getName() {
+        return "Order placed";
+    }
+}
+
+class PreparingFoodState implements OrderState {
+    @Override
+    public void next(OrderContext context) {
+        context.setCurrentState(new OutForDeliveryState());
+        System.out.println("Order status changed to `Out for delivery`");
+    }
+
+    @Override
+    public void cancel(OrderContext context) {
+        context.setCurrentState(new OrderCancelledState());
+        System.out.println("Order cancelled!!");
+    }
+
+    @Override
+    public String getName() {
+        return "Preparing food";
+    }
+}
+
+class OutForDeliveryState implements OrderState {
+    @Override
+    public void next(OrderContext context) {
+        context.setCurrentState(new DeliveredState());
+        System.out.println("Order status changed to `Delivered`");
+    }
+
+    @Override
+    public void cancel(OrderContext context) {
+        System.out.println("Cannot cancel. Order is out for delivery.");
+    }
+
+    @Override
+    public String getName() {
+        return "Out for delivery";
+    }
+}
+
+class DeliveredState implements OrderState {
+    @Override
+    public void next(OrderContext context) {
+        System.out.println("Have a happy meal!!");
+    }
+    
+    @Override
+    public void cancel(OrderContext context) {
+        System.out.println("Order cannot be cancelled after delivery!");
+    }
+
+    @Override
+    public String getName() {
+        return "Delivered";
+    }
+}
+
+class OrderCancelledState implements OrderState {
+    @Override
+    public void next(OrderContext context) {
+        System.out.println("Cancelled order cannot move to next state.");
+    }
+
+    @Override
+    public void cancel(OrderContext context) {
+        System.out.println("Order is already cancelled.");
+    }
+
+    @Override
+    public String getName() {
+        return "Order cancelled";
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        OrderContext order = new OrderContext();
+
+        System.out.println("Initial State: " + order.getCurrentState());
+
+        order.next();
+        order.next();
+        order.next();
+
+        order.cancel();
+
+        System.out.println("Final State: " + order.getCurrentState());
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ src/Template/Main.java
@@ -0,0 +1,78 @@
+import java.util.*;
+
+abstract class NotificationSender {
+    public final void send(String to, String message) {
+        this.checkRateLimits(to);
+        this.validateEmailRecepient(to);
+        this.composeMessage(message);
+        this.sendNotification(to, message);
+        this.postSendAnalytics(to);
+    }
+
+    private void checkRateLimits(String to) {
+        System.out.println("Checking rate limits for: " + to);
+    }
+
+    private void validateEmailRecepient(String to) {
+        System.out.println("Validating email recipient: " + to);
+    }
+
+    protected abstract String composeMessage(String message);
+    protected abstract void sendNotification(String to, String message);
+
+    protected void postSendAnalytics(String to) {
+        System.out.println("Analytics updated for: " + to);
+    }
+}
+
+class EmailNotification extends NotificationSender {
+    @Override
+    public String composeMessage(String message) {
+        return "<html><body><p>" + message.trim() + "</p></body></html>";
+    }
+
+    @Override
+    public void sendNotification(String to, String message) {
+        System.out.println("Sending EMAIL to " + to + " with content:\n" + message);
+    }
+}
+
+// SMSNotification handles sending SMS messages
+class SMSNotification extends NotificationSender {
+    public String composeMessage(String message) {
+        return "[SMS] " + message.trim();
+    }
+
+    public void sendNotification(String to, String message) {
+        System.out.println("Sending SMS to " + to + " with message: " + message);
+    }
+
+    public void postSendAnalytics(String to) {
+        System.out.println("Custom SMS analytics for: " + to);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Create objects for both notification services
+        SMSNotification smsNotification = new SMSNotification();
+        EmailNotification emailNotification = new EmailNotification();
+
+        // Sending email notification
+        emailNotification.send("example@example.com", "Your order has been placed!");
+        
+        System.out.println(" ");
+        
+        // Sending SMS notification
+        smsNotification.send("1234567890", "Your OTP is 1234.");
+    }
+}
+
+
+
+
+
+
+
+
+
